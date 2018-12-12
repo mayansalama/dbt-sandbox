4f4e64de3ae5ab82dc00a657fb26f3de898eaa98
mayan.s <micsalama@gmail.com>
2018-12-12 13:42:51 +1100

    made generic

--- set-up/start_cloud_sql_proxy_mac.sh
+++ set-up/start_cloud_sql_proxy_mac.sh
@@ -2,4 +2,4 @@
 curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
 chmod +x cloud_sql_proxy
 
-./cloud_sql_proxy -instances=tidal-theater-197803:australia-southeast1:dbt-sandbox=tcp:5433
+./cloud_sql_proxy -instances=$1:dbt-sandbox=tcp:5433
